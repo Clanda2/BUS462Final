9e3ac6836a74234d405d36b3fddaf5a95c5b65d6
Clanda2 <149719672+Clanda2@users.noreply.github.com>
2024-03-28 12:37:56 -0700

    Create RottenTomatoes.R
    Loading rotten tomatoes movies and prelim cleaning

---
+++ RottenTomatoes.R
@@ -0,0 +1,82 @@
+
+# BUS 462 -- Final Project 
+
+######################################################### 
+
+cat("\014")  # Clear Console
+rm(list = ls(all.names = TRUE))# clear all data objects
+gc() # clear memory
+set.seed(42)
+
+install.packages("googledrive")
+
+#loading the data and packages
+
+library(dplyr)
+library(data.table)
+library(ggplot2) 
+library(tidyr)
+library(googledrive)
+
+drive_auth()
+file_id <- "https://drive.google.com/file/d/1LtLVMOV2yBkhXo-DrvXS3E5O_U_l1M0K/view?usp=drive_link"
+downloaded_file <- drive_download(as_id(file_id),overwrite = TRUE) 
+movies <- read.csv(downloaded_file$name, header = TRUE)
+rm(downloaded_file, file_id)                        
+
+names(movies)
+
+####### DATA CLEANING ######## 
+
+summary(movies) 
+str(movies) 
+
+#check each column for missing values and sort by the number of missing values 
+missing_values <- sapply(movies, function(x) sum(is.na(x) | x == ""))
+missing_values <- missing_values[order(missing_values, decreasing = TRUE)]
+missing_values 
+
+#drop the critic consensus column 
+movies_cleaned <- movies %>% select(-critics_consensus)
+names(movies_cleaned) 
+
+#counting and removing rows with any missing values 
+movies_with_na <- movies_cleaned
+movies_with_na[movies_with_na == ""] <- NA
+num_rows_with_missing_or_blank <- sum(!complete.cases(movies_with_na)) # Count the number of rows with a missing value (including blank strings) in any column
+num_rows_with_missing_or_blank # Print the number of rows with any missing value or blank strings
+rm(movies_with_na) # Remove the temporary data frame (movies_with_na
+
+#remove rows with missing values 
+movies_cleaned[movies_cleaned == ""] <- NA 
+movies_cleaned <- na.omit(movies_cleaned) # Remove rows with missing values
+
+#drop the columns that are not needed 
+movies_cleaned <- movies_cleaned %>% select(-rotten_tomatoes_link) # Drop the rotten_tomatoes_link column 
+
+rm(missing_values, num_rows_with_missing_or_blank)
+
+
+#ensure columns are atomic (resulting DF only used for analysis on genre, actors, authors and directors as it creates duplicates)  
+movies_long_step_1 <- movies_cleaned %>% 
+  separate_rows(authors, sep = ",\\s*") %>% 
+  mutate(authors = trimws(authors))
+
+movies_long_step_2 <- movies_long_step_1 %>% 
+  separate_rows(genres, sep = ",\\s*") %>% 
+  mutate(genres = trimws(genres))
+
+movies_long_step_3 <- movies_long_step_2 %>% 
+  separate_rows(actors, sep = ",\\s*") %>% 
+  mutate(actors = trimws(actors))
+
+movies_long <- movies_long_step_3 %>% 
+  separate_rows(directors, sep = ",\\s*") %>% 
+  mutate(directors = trimws(directors)) 
+
+rm(movies_long_step_1, movies_long_step_2, movies_long_step_3) # Remove the temporary data frames (movies_long_step_1, movies_long_step_2, movies_long_step_3)
+
+
+
+
+
